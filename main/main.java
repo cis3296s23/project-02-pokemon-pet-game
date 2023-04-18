fd2c80c618605849b8e7f0ca0f7ebf8638d2f7a6
NTRachel <to.rachel.n@gmail.com>
2023-04-17 21:38:27 -0400

    Added unit tests for keyboard input

--- main/main.java
+++ main/main.java
@@ -1,37 +1,3 @@
-/*
-package main;
-
-import javax.swing.*;
-
-public class main {
-    public static void main(String[] args) {
-        // Set up JFrame object
-        JFrame window = new JFrame();
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setResizable(false);
-
-        ImageIcon windowIcon = new ImageIcon("main/windowIcon.png");
-        window.setIconImage(windowIcon.getImage());
-
-        window.setTitle("Pokemon Pet Game");
-        window.setLocationRelativeTo(null);
-        window.setVisible(true);
-
-        // Create our gamePanel (basically the main GUI) and add it to our JFrame
-        gamePanel gamePanel = new gamePanel();
-        window.add(gamePanel);
-        window.pack(); // Sizes the window so all contents are at the preferred size
-
-        gamePanel.startGameThread();
-    }
-}
-
-*/
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
-//
-
 package main;
 
 import java.awt.Component;
