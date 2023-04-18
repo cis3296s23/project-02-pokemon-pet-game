28367ec6e739d03781ee0617fb8ad5f43730c298
NTRachel <to.rachel.n@gmail.com>
2023-04-17 20:45:28 -0400

    Created unit tests for keyboard input

---
+++ main/mainTest.java
@@ -0,0 +1,86 @@
+package main;
+
+import org.junit.Test;
+import java.awt.event.KeyEvent;
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+
+
+public class mainTest {
+
+    @Test
+    public void testKeyPressLeft(){
+            KeyEventDispatcher keyEventDispatcher = new KeyEventDispatcher() {
+                @Override
+                public boolean dispatchKeyEvent(KeyEvent event) {
+                    if (event.getID() == KeyEvent.KEY_PRESSED) {
+                        if (event.getKeyCode() == KeyEvent.VK_A) {
+                            System.out.println("A key is pressed");
+                        }
+                    }
+                    return false;
+                }
+            };
+            KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(keyEventDispatcher);
+        }
+
+    @Test
+    public void testKeyPressRight(){
+        KeyEventDispatcher keyEventDispatcher = new KeyEventDispatcher() {
+        @Override
+            public boolean dispatchKeyEvent(KeyEvent event) {
+                if (event.getID() == KeyEvent.KEY_PRESSED) {
+                    if (event.getKeyCode() == KeyEvent.VK_D) {
+                        System.out.println("D key is pressed");
+                                }
+                            }
+                            return false;
+                        }
+                    };
+            
+                    KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(keyEventDispatcher);
+                }
+
+    @Test
+    public void testKeyPressSpace(){
+                KeyEventDispatcher keyEventDispatcher = new KeyEventDispatcher() {
+                    @Override
+                    public boolean dispatchKeyEvent(KeyEvent event) {
+                        if (event.getID() == KeyEvent.KEY_PRESSED) {
+                            if (event.getKeyCode() == KeyEvent.VK_SPACE) {
+                                System.out.println("Space key is pressed");
+                            }
+                        }
+                        return false;
+                    }
+                };
+
+                KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(keyEventDispatcher);
+            }
+
+    @Test
+    public void testKeyPressSpeak(){
+                KeyEventDispatcher keyEventDispatcher = new KeyEventDispatcher() {
+                    @Override
+                    public boolean dispatchKeyEvent(KeyEvent event) {
+                        if (event.getID() == KeyEvent.KEY_PRESSED) {
+                            if (event.getKeyCode() == KeyEvent.VK_S) {
+                                System.out.println("Speak key is pressed");
+                            }
+                        }
+                        return false;
+                    }
+                };
+        
+                KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(keyEventDispatcher);
+            }
+        
+        }
+
+            
+
+
+
+
+
+
