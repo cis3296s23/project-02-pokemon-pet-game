7189bf015359a6d1bda353144ca780bca1ba2266
Harsh Patel <tui47446@temple.edu>
2023-04-18 13:21:08 -0400

    Added a feature which will decrease the health bar, hunger bar, and happiness bar over time.

--- main/gamePanel.java
+++ main/gamePanel.java
@@ -3,7 +3,7 @@
 // (powered by FernFlower decompiler)
 //
 
-
+/*
 package main;
 
 import java.awt.*;
@@ -33,7 +33,7 @@ public class gamePanel extends JPanel implements Runnable {
     private Image cloud2;
     private Image cloud3;
     public keyHandler kh = new keyHandler();
-    public playerUI UI = new playerUI();
+    public playerUI UI = new playerUI(this);
     private double hungerDecrementTime = 0;
     private double happinessDecrementTime = 0;
     private double healthDecrementTime = 0;
@@ -206,4 +206,160 @@ public class gamePanel extends JPanel implements Runnable {
         g2.dispose();
     }
 
+}
+*/
+
+package main;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.plaf.DimensionUIResource;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.*;
+
+public class gamePanel extends JPanel implements Runnable {
+    public Thread gameThread;
+    public int petX = 512;
+    public int petY = 230;
+    public int petSpeed = 10;
+    public double jumpSpeed = -10;
+    public boolean isJumping = false;
+    private Image cloud1;
+    private Image cloud2;
+    private Image cloud3;
+    public keyHandler kh = new keyHandler();
+    public playerUI UI = new playerUI(this);
+
+    private JLabel currencyLabel;
+
+    public void drawImageCloud(Graphics2D g2, Image cloud, int x, int y, int width, int height) {
+        g2.drawImage(cloud, x, y, width, height, null);
+    }
+    public gamePanel() {
+        this.setPreferredSize(new DimensionUIResource(1024, 576));
+        Color sky = new Color(28, 235, 235);
+        this.setBackground(sky);
+        this.setDoubleBuffered(true); // May improve performance
+        this.addKeyListener(kh);
+        this.add(UI);
+        this.setFocusable(true);
+        // Load Cloud images
+        ImageIcon cloudIcon1 = new ImageIcon("images/cloud_01.png");
+        ImageIcon cloudIcon2 = new ImageIcon("images/cloud_02.png");
+        ImageIcon cloudIcon3 = new ImageIcon("images/cloud_03.png");
+        cloud1 = cloudIcon1.getImage();
+        cloud2 = cloudIcon2.getImage();
+        cloud3 = cloudIcon3.getImage();
+    }
+    public void startGameThread() {
+        this.requestFocus();
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+    @Override
+    public void run() {
+        int FPS = 60;
+        // Set up game loop to slow game down
+        // Set our time step to run in 60 FPS, game should update every .016 seconds
+        // instead of every nanosecond
+        final double TIME_STEP = 1.0 / FPS;
+        double accumulatedTime = 0;
+        long currentTime = System.nanoTime();
+        long lastTime = System.nanoTime();
+        // Game loop
+        // Every time we get to our time step update the game and reset accumulated time
+        // vairable, ensure game runs smoothly regardless of system
+        while (gameThread != null) {
+            // Calculate elapsed time since last update
+            currentTime = System.nanoTime();
+            double elapsedTime = (currentTime - lastTime) / 1000000000.0;
+            lastTime = currentTime;
+            accumulatedTime += elapsedTime;
+            // Update game logic and reset accumulatedTime
+            while (accumulatedTime >= TIME_STEP) {
+                try {
+                    update(TIME_STEP);
+                } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                accumulatedTime -= TIME_STEP;
+            }
+            repaint();
+        }
+    }
+    public void update(double timeStep) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        int gravity = 15;
+        int jumpHeight = 10;
+        // Move to the left
+        if (kh.leftPressed == true) {
+            petX = petX - petSpeed;
+        }
+        // Move to the right
+        if (kh.rightPressed == true) {
+            petX = petX + petSpeed;
+        }
+        // Jump
+        if (kh.jumpPressed == true && !isJumping) {
+            // Set isJumping to true so we cant jump multiple times in air
+            isJumping = true;
+            petY -= jumpHeight;
+            jumpSpeed = -10;
+            File file = new File("main/jump.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        }
+        // If jumping fall back down
+        if (isJumping) {
+            petY += (int) jumpSpeed;
+            jumpSpeed += gravity * timeStep;
+            // Don't fall past default height and set to false so we can jump again
+            if (petY >= 230) {
+                petY = 230;
+                isJumping = false;
+            }
+        }
+        // Make Noise
+        if (kh.speakPressed == true) {
+            File file = new File("main/eevee.wav");
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStream);
+            clip.start();
+        }
+        if (petX > 1024) {
+            petX = -200;
+        }
+        if (petX < -200) {
+            petX = 1024;
+        }
+    }
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+        // Draw Grass
+        g2.setColor(new Color(16, 199, 59));
+        g2.fillRect(0, 400, 1024, 300);
+        // Draw cloud images
+        drawImageCloud(g2, cloud1, 120, 70, 200, 100);
+        drawImageCloud(g2, cloud2, 450, 120, 200, 100);
+        drawImageCloud(g2, cloud3, 750, 70, 190, 110);
+        // Draw eevee
+        ImageIcon ii = new ImageIcon("main/windowIcon.png");
+        Image eevee = ii.getImage();
+        g2.drawImage(eevee, petX, petY, 64 * 4, 64 * 4, null);
+
+        // Draw Currency label
+        g2.setColor(Color.BLACK);
+        g2.setFont(new Font("Arial", Font.BOLD, 26));
+        g2.drawString("Money: " + currency.balance, 456, 560);
+
+        // Draw UI
+        UI.paint(g2);
+
+        g2.dispose();
+    }
 }
--- main/playerUI.java
+++ main/playerUI.java
@@ -1,3 +1,4 @@
+/*
 package main;
 
 import java.awt.*;
@@ -18,13 +19,15 @@ public class playerUI extends JPanel {
     int hunger = 100;
     int happiness = 100;
 
+    private gamePanel gamePanel;
     private JButton shopButton;
 
-    public playerUI() {
+    public playerUI(gamePanel gamePanel) {
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
         this.setPreferredSize(screenSize);
         this.setOpaque(false);
         this.setLayout(new BorderLayout());
+        this.gamePanel = gamePanel;
 
         // Initialize shop button and open shop window on click
         shopButton = new JButton("Shop"); // Set button text to "Shop"
@@ -37,7 +40,7 @@ public class playerUI extends JPanel {
         shopButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 System.out.println("My name Jeff");
-                shop shopWindow = new shop();
+                shop shopWindow = new shop(gamePanel);
                 shopWindow.setVisible(true);
             }
         });
@@ -124,4 +127,164 @@ public class playerUI extends JPanel {
         g2.drawString(happinessLabel, labelX, labelY);
     }
 
+}
+*/
+
+package main;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.*;
+import javax.swing.plaf.DimensionUIResource;
+
+public class playerUI extends JPanel {
+    int health = 100;
+    int hunger = 100;
+    int happiness = 100;
+    private int happinessDecreaseCounter = 0;
+    private gamePanel gamePanel;
+    private JButton shopButton;
+    private Timer timer;
+
+    public playerUI(gamePanel gamePanel) {
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setPreferredSize(screenSize);
+        this.setOpaque(false);
+        this.setLayout(new BorderLayout());
+        this.gamePanel = gamePanel;
+
+        // Initialize shop button and open shop window on click
+        shopButton = new JButton("Shop"); // Set button text to "Shop"
+        shopButton.setPreferredSize(new Dimension(100, 50));
+        shopButton.setFocusPainted(false);
+        shopButton.setOpaque(false);
+
+        this.add(shopButton, BorderLayout.NORTH);
+
+        shopButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("My name Jeff");
+                shop shopWindow = new shop(gamePanel);
+                shopWindow.setVisible(true);
+            }
+        });
+        // Initialize the timer
+        int initialDelay = 1000; // 1000 milliseconds = 1 second
+        int delayBetweenCalls = 1000; // Decrease the bars every second
+        timer = new Timer(initialDelay, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                decreaseBars();
+            }
+        });
+        timer.setDelay(delayBetweenCalls);
+        timer.start();
+    }
+
+    private void decreaseBars() {
+        int healthDecrease = 1; // Adjust this value to change the rate of health decrease
+        int hungerDecrease = 1; // Adjust this value to change the rate of hunger decrease
+        int happinessDecrease = 1; // Adjust this value to change the rate of happiness decrease
+
+        updateHealth(health - healthDecrease);
+        updateHunger(hunger - hungerDecrease);
+
+        // Update the happinessDecreaseCounter
+        happinessDecreaseCounter++;
+
+        // Decrease happiness every 3 ticks
+        if (happinessDecreaseCounter % 3 == 0) {
+            updateHappiness(happiness - happinessDecrease);
+        }
+    }
+
+
+
+
+    public void updateHealth(int health) {
+        this.health = health;
+        repaint();
+    }
+
+    public void updateHunger(int hunger) {
+        this.hunger = hunger;
+        repaint();
+    }
+
+    public void updateHappiness(int happiness) {
+        this.happiness = happiness;
+        repaint();
+    }
+
+    public void paintComponent(Graphics g) {
+        int statusBarWidth = 200;
+        int statusBarHeight = 20;
+        int cornerRadius = 5;
+
+        super.paintComponent(g);
+        Graphics2D g2 = (Graphics2D) g;
+
+        // Draw health bar
+        int healthBarX = 20;
+        int healthBarY = 310;
+        GradientPaint healthGradient = new GradientPaint(0, healthBarY, Color.RED, 0, healthBarY + statusBarHeight, Color.RED);
+
+        g2.setPaint(healthGradient);
+        g2.fillRoundRect(healthBarX, healthBarY, (int) (health * (statusBarWidth / 100.0)), statusBarHeight, cornerRadius, cornerRadius);
+        g2.setColor(Color.BLACK);
+        g2.drawRoundRect(healthBarX, healthBarY, statusBarWidth, statusBarHeight, cornerRadius, cornerRadius);
+
+        // Draw hunger bar
+        int hungerBarX = 20;
+        int hungerBarY = 340;
+        GradientPaint hungerGradient = new GradientPaint(0, hungerBarY, Color.ORANGE, 0, hungerBarY + statusBarHeight, Color.GRAY);
+
+        g2.setPaint(hungerGradient);
+        g2.fillRoundRect(hungerBarX, hungerBarY, (int) (hunger * (statusBarWidth / 100.0)), statusBarHeight, cornerRadius, cornerRadius);
+        g2.setColor(Color.BLACK);
+        g2.drawRoundRect(hungerBarX, hungerBarY, statusBarWidth, statusBarHeight, cornerRadius, cornerRadius);
+
+        // Draw happiness bar
+        int happinessBarX = 20;
+        int happinessBarY = 370;
+        GradientPaint happinessGradient = new GradientPaint(0, happinessBarY, Color.GREEN, 0, happinessBarY + statusBarHeight, Color.GRAY);
+
+        g2.setPaint(happinessGradient);
+        g2.fillRoundRect(happinessBarX, happinessBarY, (int) (happiness * (statusBarWidth / 100.0)), statusBarHeight, cornerRadius, cornerRadius);
+        g2.setColor(Color.BLACK);
+        g2.drawRoundRect(happinessBarX, happinessBarY, statusBarWidth, statusBarHeight, cornerRadius, cornerRadius);
+
+        g2.setColor(Color.BLACK);
+        FontMetrics fm = g2.getFontMetrics();
+        int labelHeight = fm.getHeight();
+
+        // Draw health label
+        String healthLabel = "Health: " + health;
+        int labelWidth = fm.stringWidth(healthLabel);
+        int labelX = healthBarX + (statusBarWidth - labelWidth) / 2;
+        int labelY = healthBarY + (statusBarHeight - labelHeight) / 2 + fm.getAscent();
+        g2.drawString(healthLabel, labelX, labelY);
+
+        // Draw hunger label
+        String hungerLabel = "Hunger: " + hunger;
+        labelWidth = fm.stringWidth(hungerLabel);
+        labelX = hungerBarX + (statusBarWidth - labelWidth) / 2;
+        labelY = hungerBarY + (statusBarHeight - labelHeight) / 2 + fm.getAscent();
+        g2.drawString(hungerLabel, labelX, labelY);
+
+        // Draw happiness label
+        String happinessLabel = "Happiness: " + happiness;
+        labelWidth = fm.stringWidth(happinessLabel);
+        labelX = happinessBarX + (statusBarWidth - labelWidth) / 2;
+        labelY = happinessBarY + (statusBarHeight - labelHeight) / 2 + fm.getAscent();
+        g2.drawString(happinessLabel, labelX, labelY);
+    }
 }
